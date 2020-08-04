88df8678a1731e94bcca39367b4616b78996518e
HaoCold <xjh093@126.com>
2020-08-04 15:15:56 +0800

    Add files via upload

---
+++ UIButtonGradientBackgroundColor/UIButton+GradientBackgroundColor.h
@@ -0,0 +1,19 @@
+//
+//  UIButton+GradientBackgroundColor.h
+//  JHKit
+//
+//  Created by HaoCold on 2020/3/14.
+//  Copyright © 2020 HaoCold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIButton (GradientBackgroundColor)
+
+- (void)gradientBackgroundColor:(NSArray <UIColor *>*)colors;
+
+@end
+
+NS_ASSUME_NONNULL_END
